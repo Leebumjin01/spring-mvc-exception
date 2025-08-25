082824ab21ba6e4917bf92aae60e8cb93f9d8d0f
이범진 <lbj0797@dataeum.kr>
2025-08-25 14:52:22 +0900

    HandlerExceptionResolver 활용

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,6 +1,7 @@
 package hello.exception.api;
 
 
+import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -18,9 +19,13 @@ public class ApiExceptionController {
         if (id.equals("ex")) {
             throw new RuntimeException("잘못 된 사용자");
         }
-        
-        if(id.equals("bad")) {
+
+        if (id.equals("bad")) {
             throw new IllegalArgumentException("잘못된 입력 값");
+        }
+
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
         }
 
         return new MemberDto(id, "hello " + id);
